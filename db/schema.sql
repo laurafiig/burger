052d08602512c082e4d2327a002f6f1cd562fe04
Laura Fiig <Laura Fiig>
2016-12-12 22:02:23 -0600

    .sql files populated.  Next create database, server.js, sql server connection

---
+++ db/schema.sql
@@ -0,0 +1,10 @@
+CREATE database burgers_db;
+
+USE burgers_db;
+CREATE TABLE burgers (
+	id INTEGER AUTO_INCREMENT NOT NULL,
+	PRIMARY KEY (id),
+	burger_name VARCHAR(100) NULL,
+	devoured BOOLEAN,	
+	date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+)
---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("cheeseburger", false),
+("black bean burger", false),
+("turkey burger", false)
