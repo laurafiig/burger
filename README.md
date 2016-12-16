a19c48a7a87e4f4009ceadf1e5f46419f489cbbe
Laura Fiig <Laura Fiig>
2016-12-15 22:45:19 -0600

    jaws db info added

--- README.md
+++ README.md
@@ -7,3 +7,6 @@ Uses MySQL, Node, Express, Handlebars and a homemade ORM.
 Follows the MVC design pattern
 
 Uses Node and MySQL to query and route data, and Handlebars to generate HTML.
+
+https://git.heroku.com/damp-island-37250.git
+
--- config/connection.js
+++ config/connection.js
@@ -2,14 +2,19 @@
 var mysql = require("mysql");
 
 // connect
-var connection = mysql.createConnection({
+var connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
   password: "",
   database: "burgers_db"
-});
-
+  });
+};
 //make connection
 connection.connect(function(err) {
   if (err) {
